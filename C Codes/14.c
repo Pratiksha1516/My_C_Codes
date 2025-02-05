bd4c896384c4faca5c7e51e1ae01f94d1dd3c39f
Pratiksha1516 <prateekshappatil@gmail.com>
2025-02-05 16:24:33 +0530

    2

---
+++ C Codes/14.c
@@ -0,0 +1,38 @@
+//  on number
+#include<stdio.h>           
+#include<stdbool.h> //  checck whether true or false
+
+bool CheckDivisible(int iVal)
+{
+        int iAns = 0;       //local variable
+        
+        if(((iVal % 3) == 0  && (iVal %5) == 0))
+        {
+            return true;    // this value will go to the bFlag variable
+        }
+        else
+        {
+            return false;
+        }
+
+}
+int main()
+{
+    int icount = 0;     // to take ip from user
+    bool bFlag = true; //to store the fun's return value ( os consider internally true=1; false=0 )
+
+    printf("Enter number:  ");
+    scanf("%d",&icount);
+
+    bFlag = CheckDivisible(icount);
+    if(bFlag == true)
+    {
+        printf("%d is divisible by 3 and 5\n",icount);
+    }
+    else
+    {
+        printf("%d is not divisible by 3 and 5\n",icount);
+    }
+
+    return 0;
+}
---
+++ C Codes/9for.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int icount = 0;
+    for(icount = 1; icount <=5; icount++)
+    {
+        printf("%d\n",icount);
+    }
+
+    return 0;
+
+}
